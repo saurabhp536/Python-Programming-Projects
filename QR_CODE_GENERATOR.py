00ab397c34a15e107f2f375b1721c7f54333395a
Saurabh Patil <100773962+saurabhp536@users.noreply.github.com>
2024-05-03 13:10:44 +0530

    QR Code Generator

---
+++ QR_CODE_GENERATOR.py
@@ -0,0 +1,15 @@
+import qrcode
+
+# Data to encode in the QR code
+data = "https://www.youtube.com/"
+
+# Generate QR code
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("my_qrcode.png")
